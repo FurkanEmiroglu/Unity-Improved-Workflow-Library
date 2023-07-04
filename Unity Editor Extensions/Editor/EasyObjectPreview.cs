77e9cd2cc01b6e980d00ccea98700e470789c37f
Furkan Emiroglu <emiroglu.furkan@gmail.com>
2023-07-04 14:34:05 +0300

    object previewer namespace fix

--- Unity Editor Extensions/Editor/EasyObjectPreview.cs
+++ Unity Editor Extensions/Editor/EasyObjectPreview.cs
@@ -1,68 +1,71 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class EasyObjectPreview : EditorWindow
+namespace ImprovedWorkflow.UnityEditorExtensions
 {
-    private static EasyObjectPreview window;
-    private static Object selectedObject;
+    public class EasyObjectPreview : EditorWindow
+    {
+        private static EasyObjectPreview window;
+        private static Object selectedObject;
 
-    private Editor _previewEditor;
+        private Editor _previewEditor;
 
-    private static Vector2 _windowSize = new Vector2(500, 500);
+        private static Vector2 _windowSize = new Vector2(500, 500);
     
-    [MenuItem("Tools/Easy Object Preview %SPACE")]
-    private static void Init()
-    {
-        if (window == null)
+        [MenuItem("Tools/Easy Object Preview %SPACE")]
+        private static void Init()
         {
-            ShowWindow();
+            if (window == null)
+            {
+                ShowWindow();
+            }
         }
-    }
 
-    private static void ShowWindow()
-    {
-        selectedObject = Selection.activeObject;
+        private static void ShowWindow()
+        {
+            selectedObject = Selection.activeObject;
             
-        EasyObjectPreview wind = GetWindow<EasyObjectPreview>(true, string.Empty, true);
+            EasyObjectPreview wind = GetWindow<EasyObjectPreview>(true, string.Empty, true);
             
-        wind.maxSize = new Vector2(_windowSize.x, _windowSize.y);
-        wind.minSize = new Vector2(_windowSize.x, _windowSize.y);
+            wind.maxSize = new Vector2(_windowSize.x, _windowSize.y);
+            wind.minSize = new Vector2(_windowSize.x, _windowSize.y);
             
-        wind.ShowPopup();
+            wind.ShowPopup();
     
-        window = wind;
-    }
+            window = wind;
+        }
     
-    private void OnGUI()
-    {
-        PreviewObject();
-        CheckCloseInput();
-    }
-
-    private void CheckCloseInput()
-    {
-        var currentEvent = Event.current;
-        
-        if (currentEvent.type == EventType.KeyUp && currentEvent.keyCode == KeyCode.Space)
+        private void OnGUI()
         {
-            window.Close();
+            PreviewObject();
+            CheckCloseInput();
         }
 
-        if (focusedWindow != window)
+        private void CheckCloseInput()
         {
-            window.Close();
+            var currentEvent = Event.current;
+        
+            if (currentEvent.type == EventType.KeyUp && currentEvent.keyCode == KeyCode.Space)
+            {
+                window.Close();
+            }
+
+            if (focusedWindow != window)
+            {
+                window.Close();
+            }
         }
-    }
     
-    private void PreviewObject()
-    {
-        if (selectedObject == null) return;
-        
-        if (_previewEditor == null)
+        private void PreviewObject()
         {
-            _previewEditor = Editor.CreateEditor(selectedObject);
-        }
+            if (selectedObject == null) return;
         
-        _previewEditor.OnPreviewGUI(GUILayoutUtility.GetRect(_windowSize.x, _windowSize.y), new GUIStyle());
+            if (_previewEditor == null)
+            {
+                _previewEditor = Editor.CreateEditor(selectedObject);
+            }
+        
+            _previewEditor.OnPreviewGUI(GUILayoutUtility.GetRect(_windowSize.x, _windowSize.y), new GUIStyle());
+        }
     }
 }
